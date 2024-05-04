0e61d84eaa89ca1289ee9c1fe9d881aeed7758e8
Ale_Ksandr <dark7000000001@gmail.com>
2024-05-04 22:49:23 +0700

    Added _get_group for Handler and wrote a request to collection

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from dataclasses import dataclass
 import pandas as pd
-
+from mongo import collection
 
 @dataclass
 class Request:
@@ -20,8 +20,55 @@ class Handler:
         return f'{self.dt_from} - {self.dt_upto}\n{self.group_type=}'
     
     async def request_to_responce(self) -> dict[str, list]:
-        return {}
+        labels = await self._get_labels()
+        dataset = [0] * len(labels)
+        response = {"dataset": dataset, "labels": labels}
+        
+        
+        pipeline = [
+            {"$match": {"dt": {"$gte": self.dt_from, "$lte": self.dt_upto}}},
+            {"$group": await self._get_group()},
+            {"$sort": {"_id": 1}}
+        ]
+
+        result = collection.aggregate(pipeline)
+        
+        for res in result:
+            index = response['labels'].index(res['_id']['datetime'])
+            response['dataset'][index] = res['total_value']
+            
+        return response
     
+    async def _get_group(self) -> dict:
+        match self.group_type:
+            case 'month':
+                return {
+                    '_id': {
+                            'month': {'$month':'$dt'}, 
+                            'datetime':{"$dateToString": {"format": "%Y-%m-01T00:00:00", "date": "$dt"}},
+                        }, 
+                        "total_value": {"$sum": "$value"}
+                    }
+            case 'day':
+                return {
+                    '_id': { 
+                            'day': {'$dayOfMonth':'$dt'}, 
+                            'datetime':{"$dateToString": {"format": "%Y-%m-%dT00:00:00", "date": "$dt"}},
+                        }, 
+                        "total_value": {"$sum": "$value"}
+                    }
+            case 'hour':
+                return {
+                    '_id': { 
+                            'hour': {'$hour':'$dt'}, 
+                            'datetime':{"$dateToString": {"format": "%Y-%m-%dT%H:00:00", "date": "$dt"}},
+                        }, 
+                        "total_value": {"$sum": "$value"}
+                    }
+            case _:
+                raise ValueError('Invalid group!')
+
+      
     async def _get_labels(self) -> list[str]:
         match self.group_type:
             case 'month':
